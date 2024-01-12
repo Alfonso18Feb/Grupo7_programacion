e050d7580c5e1fff47844789052bab75acdec8f2
Alfonso Criado-Pou Castellanos <146075356+Alfonso18Feb@users.noreply.github.com>
2024-01-12 19:49:29 +0100

    ANADIR COMENTARIOS

--- Rana_Laberinto.py
+++ Rana_Laberinto.py
@@ -1,12 +1,13 @@
-#!/bin/python3
+#Importamos las utilidades necesarias para la ejecución del programa
 
 import math
 import os
 import random
 import re
 import sys
-
+#Comienza la parte principal (main) del programa
 if __name__ == '__main__':
+        #Definimos el laberinto indicando el número de filas, colunas y túneles que vamos a poner.
     print("Define el laberinto")
     n = int(input("¿Número de filas?"))
     m = int(input("¿Número de columnas?"))
@@ -26,11 +27,11 @@ if __name__ == '__main__':
         t_exit[(i1, j1)] = (i2, j2)
         t_exit[(i2, j2)] = (i1, j1)
 
-    states = [(-1, -1)]  # Death state.
+    states = [(-1, -1)] # Aquí guardamos todas las posiciones a las que puede moverse la rana.
     state2idx = dict()
-    v = [0.0]  # State values, i.e. probability.
+    v = [0.0]  # Valores de probabilidad.
     s_init = -1
-    transitions = [[(0, 1.0)]]  # Self loop when dead.
+    transitions = [[(0, 1.0)]] # Valores de los posilbles saltos de la rana.
     
     for i1 in range(n):
         for j1 in range(m):
@@ -42,7 +43,7 @@ if __name__ == '__main__':
                 state2idx[(i1, j1)] = 0       
             else:
                 assert False, x
-    
+        #print (state2idx) print para visualizar el funcionamiento correcto de los datos.
     for i1 in range(n):
         for j1 in range(m):
             x = board[i1][j1]
@@ -50,26 +51,31 @@ if __name__ == '__main__':
             if x in ["A", "%", "O"]:
                 if x == "A":
                     s_init = state_idx
-                    v.append(0.0)
+                    v.append(0.0) #Valor cuando no sale la rana estando en su posición inicial.
                 elif x == "%":
-                    v.append(1.0)
-                    transitions.append([(state_idx, 1.0)])  # Self loop when exit.
+                    v.append(1.0) #Valor cuando sale la rana.
+                    transitions.append([(state_idx, 1.0)])  
                 elif x == "O":
-                    v.append(0.0)
+                    v.append(0.0) #Valor cuando no sale la rana estando en una casilla libre.
                 else:
                     assert False, x
                 if x in ["A", "O"]:
                     i2, j2 = t_exit[(i1, j1)] if (i1, j1) in t_exit else (i1, j1)
                     succs = []
-                    deaths = 0
+                    deaths = 0 #Variable para contabilizar las muertes de la rana.
+                    """Desde una posición A (inicial) o O (casilla libre)
+                        este (for) evalua los movimientos avanzando la rana
+                        una casilla a la derecha (0,1), izquierda(0,-1), 
+                        arriba(1,0) o abajo(-1,0)."""
                     for a, b in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                         i3, j3 = i2 + a, j2 + b
                         if (0 <= i3 < n) and (0 <= j3 < m):
                             y = board[i3][j3]
                             if y in ["A", "%", "O"]:
-                                succs.append((i3, j3))
+                                succs.append((i3, j3)) #En el salto ha llegado a la casilla de inicio A, la salida % o una libre O.
                             elif y == "*":
-                                deaths += 1
+                                deaths += 1 #En el salto ha encontrado una mina y muere.
+                    """En este (if) se evalúa las veces que no ha muerto y las que si."""
                     if len(succs) == 0:
                         transitions.append([(0, 1.0)])
                     else:
@@ -77,7 +83,8 @@ if __name__ == '__main__':
                         if deaths > 0:
                             t.append((0, deaths / (len(succs) + deaths)))
                         transitions.append(t)
-          
+    #print(states) #print de control muestra las posiciones donde no hay muro.
+    #print(transitions) #print de control muestra los posibles saltos con su probabilidad.
     while True:
         v_old = v.copy()
         for state in range(len(states)):
@@ -86,13 +93,14 @@ if __name__ == '__main__':
                 x += v[succ] * prob
             v[state] = x
         keep_going = False
+        #for para salir del while
         for state in range(len(states)):
             if abs(v[state] - v_old[state]) > 1e-10:
                 keep_going = True
                 break
         if not keep_going:
             break
-
+    #Imprimimos el resultado expresando la probilidad de que la rana salga del laberinto.
     print("La probabilidad de que salga la rana es del ",v[s_init]*100," %.")
 
 
