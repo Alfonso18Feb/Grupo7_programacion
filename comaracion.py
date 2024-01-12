2e039614c56757a26ab147040406a01a99a5aa31
Alfonso Criado-Pou Castellanos <146075356+Alfonso18Feb@users.noreply.github.com>
2024-01-12 19:01:56 +0100

    Añadir comentarios

--- comaracion.py
+++ comaracion.py
@@ -1,19 +1,27 @@
 import os
-
+'''
+La funcion que permite comparar las notas de lucia y carlos
+'''
 def compareTriplets(a, b):
     carlos_score = 0
     lucia_score = 0
 
     for i in range(len(a)):
+        #el rango es la longitud de la lista a
         if a[i] > b[i]:
             lucia_score += 1
+             #compara y añade un punto a lucia
         elif a[i] < b[i]:
             carlos_score += 1
+            #compara y añade un punto a carlos
         else:
             lucia_score and carlos_score ==0
+            #no añade puntos
 
-    return [lucia_score, carlos_score]
-
+    return [lucia_score, carlos_score] #devuelve el resultado de lucia y carlos
+'''
+Aqui se crean las listas a y b (a lista lucia y b de carlos) y tiene una excepción si escribes mal las lista
+'''
 if __name__ == '__main__':
     # Cambia el nombre del archivo de salida según tus necesidades
     output_file_path = "output.txt"
@@ -26,7 +34,3 @@ if __name__ == '__main__':
         print("Error: Ingrese solo números válidos.")
         exit()
     print(compareTriplets(a, b))
-    #    with open(output_file_path, 'w') as fptr:
-    #        result = print(compareTriplets(a, b))
-    #        fptr.write(' '.join(map(str, result)))
-    #        fptr.write('\n')
