87cd5a3ffeec0f991886fbe5ecdbf819555df9a2
Alfonso Criado-Pou Castellanos <146075356+Alfonso18Feb@users.noreply.github.com>
2024-01-12 12:01:39 +0100

    elimine comentarios
    inecesarios

--- staircase.py
+++ staircase.py
@@ -4,26 +4,7 @@ import random
 import re
 import sys
 
-#
-# Complete the 'staircase' function below.
-#
-# The function accepts INTEGER n as parameter.
-#
-#def staircase(n):
-# Write your code here
- #   n = int(input("elige el numero de escalones:"))
-  #  return(n)
-#    if n==1:
-#    n == int(input().strip())
- #   staircase(n)
-#     for i in range(1, n + 1):
-#         spaces = ' ' * (n - i)
-#         hashes = '#' * i
-#         print(spaces + hashes)
 
-# if __name__ == '__main__':
-#     n = int(input().strip())
-#     staircase(n)
 '''
 Función que pide el numero de # y te lo devuelve con espacios
 '''
